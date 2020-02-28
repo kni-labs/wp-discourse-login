b15564c27632330a653f060529175ba9cf1b8634
Jake Ols <jake@kurtnoble.com>
2020-02-28 10:35:42 -0800

    linting fixes

--- nonce.php
+++ nonce.php
@@ -6,183 +6,175 @@
 /**
  * Nonce generator
  */
-class Nonce
-{
+class Nonce {
 
-    /**
-     * Database Verson of nonce table
-     *
-     * @var string
-     */
-    private $db_version = '1.0.1';
 
-    /**
-     * Nonce Class Instance
-     *
-     * @var mixed
-     */
-    private static $instance = null;
+	/**
+	 * Database Verson of nonce table
+	 *
+	 * @var string
+	 */
+	private $db_version = '1.0.1';
 
-    /**
-     * Constructor
-     *
-     * @method __construct
-     */
-    private function __construct()
-    {
-        global $wpdb;
-        $this->wpdb = $wpdb;
+	/**
+	 * Nonce Class Instance
+	 *
+	 * @var mixed
+	 */
+	private static $instance = null;
 
-        /**
-         * The default is set to 10 minutes
-         *
-         * @var int
-         */
-        $this->nonce_life = intval(600);
+	/**
+	 * Constructor
+	 *
+	 * @method __construct
+	 */
+	private function __construct() {
+		global $wpdb;
+		$this->wpdb = $wpdb;
 
-        $this->maybe_create_db();
-    }
+		/**
+		 * The default is set to 10 minutes
+		 *
+		 * @var int
+		 */
+		$this->nonce_life = intval( 600 );
 
-    /**
-     * Singleton instance
-     *
-     * @method get_instance
-     *
-     * @return \WPDiscourse\SSOClient\Nonce       the instance.
-     */
-    public static function get_instance()
-    {
-        if (is_null(self::$instance)) {
-            self::$instance = new self();
-        }
+		$this->maybe_create_db();
+	}
 
-        return self::$instance;
-    }
+	/**
+	 * Singleton instance
+	 *
+	 * @method get_instance
+	 *
+	 * @return \WPDiscourse\SSOClient\Nonce       the instance.
+	 */
+	public static function get_instance() {
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
+		}
 
-    /**
-     * Get the Database Name
-     *
-     * @method get_table_name
-     *
-     * @return string      the db name.
-     */
-    private function get_table_name()
-    {
-        return "{$this->wpdb->prefix}wpdlg_nonce";
-    }
+		return self::$instance;
+	}
 
-    /**
-     * Db shouldn't be created/updated unless if it's an old version.
-     *
-     * @method maybe_create_db
-     */
-    private function maybe_create_db()
-    {
-        if (version_compare(get_option('wpdlg_nonce_db_version', -1), $this->db_version) !== 1) {
-            require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+	/**
+	 * Get the Database Name
+	 *
+	 * @method get_table_name
+	 *
+	 * @return string      the db name.
+	 */
+	private function get_table_name() {
+		return "{$this->wpdb->prefix}wpdlg_nonce";
+	}
 
-            $table_name = $this->get_table_name();
-            $charset = $this->wpdb->get_charset_collate();
+	/**
+	 * Db shouldn't be created/updated unless if it's an old version.
+	 *
+	 * @method maybe_create_db
+	 */
+	private function maybe_create_db() {
+		if ( version_compare( get_option( 'wpdlg_nonce_db_version', -1 ), $this->db_version ) !== 1 ) {
+			require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 
-            dbDelta(
-                "CREATE TABLE {$table_name} (
+			$table_name = $this->get_table_name();
+			$charset    = $this->wpdb->get_charset_collate();
+
+			dbDelta(
+				"CREATE TABLE {$table_name} (
 					id mediumint(9) NOT NULL AUTO_INCREMENT,
 					added_on datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
 					nonce varchar(255) DEFAULT '' NOT NULL,
 					action varchar(255) DEFAULT '' NOT NULL,
 					PRIMARY KEY  (id)
 				) $charset;"
-            );
+			);
 
-            update_option('wpdlg_nonce_db_version', $this->db_version);
-        }
+			update_option( 'wpdlg_nonce_db_version', $this->db_version );
+		}
 
-        $this->purge_expired_nonces();
-    }
+		$this->purge_expired_nonces();
+	}
 
-    /**
-     * Will purge expired nonces.
-     *
-     * @method purge_expired_nonces
-     */
-    private function purge_expired_nonces()
-    {
-        $table_name = $this->get_table_name();
+	/**
+	 * Will purge expired nonces.
+	 *
+	 * @method purge_expired_nonces
+	 */
+	private function purge_expired_nonces() {
+		$table_name = $this->get_table_name();
 
-        $expired_nonces = $this->wpdb->get_results("SELECT id FROM {$table_name} WHERE added_on < DATE_SUB(NOW(), INTERVAL {$this->nonce_life} SECOND)");
+		$expired_nonces = $this->wpdb->get_results( "SELECT id FROM {$table_name} WHERE added_on < DATE_SUB(NOW(), INTERVAL {$this->nonce_life} SECOND)" );
 
-        if (count($expired_nonces)) {
-            $expired_nonces = array_map('intval', wp_list_pluck($expired_nonces, 'id'));
-            $expired_nonces = implode(',', $expired_nonces);
-            $this->wpdb->get_results("DELETE FROM {$table_name} WHERE id IN ({$expired_nonces})");
-        }
-    }
+		if ( count( $expired_nonces ) ) {
+			$expired_nonces = array_map( 'intval', wp_list_pluck( $expired_nonces, 'id' ) );
+			$expired_nonces = implode( ',', $expired_nonces );
+			$this->wpdb->get_results( "DELETE FROM {$table_name} WHERE id IN ({$expired_nonces})" );
+		}
+	}
 
-    /**
-     * Creates a truly unique nonce based on the provided action
-     *
-     * @method create
-     *
-     * @param string|int $action Scalar value to add context to the nonce.
-     *
-     * @return string
-     */
-    public function create($action = -1)
-    {
-        $nonce = wp_hash(uniqid($action, true), 'nonce');
+	/**
+	 * Creates a truly unique nonce based on the provided action
+	 *
+	 * @method create
+	 *
+	 * @param string|int $action Scalar value to add context to the nonce.
+	 *
+	 * @return string
+	 */
+	public function create( $action = -1 ) {
+		$nonce = wp_hash( uniqid( $action, true ), 'nonce' );
 
-        $this->wpdb->insert(
-            $this->get_table_name(),
-            array(
-                'nonce' => $nonce,
-                'action' => $action,
-            ),
-            array('%s', '%s')
-        );
+		$this->wpdb->insert(
+			$this->get_table_name(),
+			array(
+				'nonce'  => $nonce,
+				'action' => $action,
+			),
+			array( '%s', '%s' )
+		);
 
-        return $nonce;
-    }
+		return $nonce;
+	}
 
-    /**
-     * Verify a nonce if it's valid and it will invalidate it
-     *
-     * @method verify
-     *
-     * @param  string     $nonce  the nonce to be validated.
-     * @param string|int $action Scalar value to add context to the nonce.
-     *
-     * @return bool
-     */
-    public function verify($nonce, $action = -1)
-    {
-        $table_name = $this->get_table_name();
+	/**
+	 * Verify a nonce if it's valid and it will invalidate it
+	 *
+	 * @method verify
+	 *
+	 * @param  string     $nonce  the nonce to be validated.
+	 * @param string|int $action Scalar value to add context to the nonce.
+	 *
+	 * @return bool
+	 */
+	public function verify( $nonce, $action = -1 ) {
+		$table_name = $this->get_table_name();
 
-        $valid_nonce = $this->wpdb->get_row($this->wpdb->prepare("SELECT id FROM {$table_name} WHERE nonce = %s AND action = %s", $nonce, $action));
+		$valid_nonce = $this->wpdb->get_row( $this->wpdb->prepare( "SELECT id FROM {$table_name} WHERE nonce = %s AND action = %s", $nonce, $action ) );
 
-        if (!empty($valid_nonce)) {
-            return (bool) $this->invalidate_nonce($valid_nonce->id);
-        }
+		if ( ! empty( $valid_nonce ) ) {
+			return (bool) $this->invalidate_nonce( $valid_nonce->id );
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    /**
-     * Delete the nonce from the DB once it is used
-     *
-     * @method invalidate_nonce
-     *
-     * @param  int $id the nonce ID that needs to be invalidated.
-     *
-     * @return boolean
-     */
-    private function invalidate_nonce($id)
-    {
-        return $this->wpdb->delete(
-            $this->get_table_name(),
-            array(
-                'id' => $id,
-            ),
-            array('%d')
-        );
-    }
+	/**
+	 * Delete the nonce from the DB once it is used
+	 *
+	 * @method invalidate_nonce
+	 *
+	 * @param  int $id the nonce ID that needs to be invalidated.
+	 *
+	 * @return boolean
+	 */
+	private function invalidate_nonce( $id ) {
+		return $this->wpdb->delete(
+			$this->get_table_name(),
+			array(
+				'id' => $id,
+			),
+			array( '%d' )
+		);
+	}
 }
--- utilities.php
+++ utilities.php
@@ -12,130 +12,127 @@ namespace WPDiscourseLogin\Utilities;
  *
  * @package WPDiscourse
  */
-class Utilities
-{
+class Utilities {
 
-    /**
-     * Validates the response from `wp_remote_get` or `wp_remote_post`.
-     *
-     * @param array $response The response from `wp_remote_get` or `wp_remote_post`.
-     *
-     * @return int
-     */
-    public static function validate($response)
-    {
-        if (empty($response)) {
 
-            return 0;
-        } elseif (is_wp_error($response)) {
+	/**
+	 * Validates the response from `wp_remote_get` or `wp_remote_post`.
+	 *
+	 * @param array $response The response from `wp_remote_get` or `wp_remote_post`.
+	 *
+	 * @return int
+	 */
+	public static function validate( $response ) {
+		if ( empty( $response ) ) {
 
-            return 0;
+			return 0;
+		} elseif ( is_wp_error( $response ) ) {
 
-            // There is a response from the server, but it's not what we're looking for.
-        } elseif (intval(wp_remote_retrieve_response_code($response)) !== 200) {
+			return 0;
 
-            return 0;
-        } else {
-            // Valid response.
-            return 1;
-        }
-    }
+			// There is a response from the server, but it's not what we're looking for.
+		} elseif ( intval( wp_remote_retrieve_response_code( $response ) ) !== 200 ) {
 
-    /**
-     * Get a Discourse user object.
-     *
-     * @param int  $user_id The WordPress user_id.
-     * @param bool $match_by_email Whether or not to attempt to get the user by their email address.
-     *
-     * @return array|mixed|object|\WP_Error
-     */
-    public static function get_discourse_user($user_id, $match_by_email = false)
-    {
-        $api_url = get_option('wpdlg_discourse_url');
-        $api_key = get_option('wpdlg_discourse_api');
+			return 0;
+		} else {
+			// Valid response.
+			return 1;
+		}
+	}
 
-        $external_user_url = esc_url_raw("{$api_url}/users/by-external/{$user_id}.json");
+	/**
+	 * Get a Discourse user object.
+	 *
+	 * @param int  $user_id The WordPress user_id.
+	 * @param bool $match_by_email Whether or not to attempt to get the user by their email address.
+	 *
+	 * @return array|mixed|object|\WP_Error
+	 */
+	public static function get_discourse_user( $user_id, $match_by_email = false ) {
+		$api_url = get_option( 'wpdlg_discourse_url' );
+		$api_key = get_option( 'wpdlg_discourse_api' );
 
-        $response = wp_remote_get(
-            $external_user_url,
-            array(
-                'headers' => array(
-                    'Api-Key' => sanitize_key($api_key),
-                    'Api-Username' => 'system',
-                ),
-            )
-        );
+		$external_user_url = esc_url_raw( "{$api_url}/users/by-external/{$user_id}.json" );
 
-        if (self::validate($response)) {
+		$response = wp_remote_get(
+			$external_user_url,
+			array(
+				'headers' => array(
+					'Api-Key'      => sanitize_key( $api_key ),
+					'Api-Username' => 'system',
+				),
+			)
+		);
 
-            $body = json_decode(wp_remote_retrieve_body($response));
-            if (isset($body->user)) {
+		if ( self::validate( $response ) ) {
 
-                return $body->user;
-            }
-        }
+			$body = json_decode( wp_remote_retrieve_body( $response ) );
+			if ( isset( $body->user ) ) {
 
-        if ($match_by_email) {
-            $user = get_user_by('id', $user_id);
+				return $body->user;
+			}
+		}
 
-            if (!empty($user) && !is_wp_error($user)) {
+		if ( $match_by_email ) {
+			$user = get_user_by( 'id', $user_id );
 
-                return self::get_discourse_user_by_email($user->user_email);
-            } else {
+			if ( ! empty( $user ) && ! is_wp_error( $user ) ) {
 
-                return new \WP_Error('wpdlg_param_error', 'There is no WordPress user with the supplied id.');
-            }
-        }
+				return self::get_discourse_user_by_email( $user->user_email );
+			} else {
 
-        return new \WP_Error('wpdlg_response_error', 'The Discourse user could not be retrieved.');
-    }
+				return new \WP_Error( 'wpdlg_param_error', 'There is no WordPress user with the supplied id.' );
+			}
+		}
 
-    /**
-     * Gets a Discourse user by their email address.
-     *
-     * @param string $email The email address to search for.
-     *
-     * @return object \WP_Error
-     */
-    public static function get_discourse_user_by_email($email)
-    {
-        $api_url = get_option('wpdlg_discourse_url');
-        $api_key = get_option('wpdlg_discourse_api');
+		return new \WP_Error( 'wpdlg_response_error', 'The Discourse user could not be retrieved.' );
+	}
 
-        $users_url = "{$api_url}/admin/users/list/all.json";
-        $users_url = esc_url_raw(
-            add_query_arg(
-                array(
-                    'email' => rawurlencode_deep($email),
-                    'filter' => rawurlencode_deep($email),
-                ),
-                $users_url
-            )
-        );
+	/**
+	 * Gets a Discourse user by their email address.
+	 *
+	 * @param string $email The email address to search for.
+	 *
+	 * @return object \WP_Error
+	 */
+	public static function get_discourse_user_by_email( $email ) {
+		$api_url = get_option( 'wpdlg_discourse_url' );
+		$api_key = get_option( 'wpdlg_discourse_api' );
 
-        $response = wp_remote_get(
-            $users_url,
-            array(
-                'headers' => array(
-                    'Api-Key' => sanitize_key($api_key),
-                    'Api-Username' => 'system',
-                ),
-            )
-        );
-        if (self::validate($response)) {
-            $body = json_decode(wp_remote_retrieve_body($response));
-            // The reqest returns a valid response even if the user isn't found, so check for empty.
-            if (!empty($body) && !empty($body[0])) {
+		$users_url = "{$api_url}/admin/users/list/all.json";
+		$users_url = esc_url_raw(
+			add_query_arg(
+				array(
+					'email'  => rawurlencode_deep( $email ),
+					'filter' => rawurlencode_deep( $email ),
+				),
+				$users_url
+			)
+		);
 
-                return $body[0];
-            } else {
+		$response = wp_remote_get(
+			$users_url,
+			array(
+				'headers' => array(
+					'Api-Key'      => sanitize_key( $api_key ),
+					'Api-Username' => 'system',
+				),
+			)
+		);
+		if ( self::validate( $response ) ) {
+			$body = json_decode( wp_remote_retrieve_body( $response ) );
+			// The reqest returns a valid response even if the user isn't found, so check for empty.
+			if ( ! empty( $body ) && ! empty( $body[0] ) ) {
 
-                // A valid response was returned, but the user wasn't found.
-                return new \WP_Error('wpdlg_response_error', 'The user could not be retrieved by their email address.');
-            }
-        } else {
+				return $body[0];
+			} else {
 
-            return new \WP_Error('wpdlg_response_error', 'An invalid response was returned when trying to find the user by email address.');
-        }
-    }
+				// A valid response was returned, but the user wasn't found.
+				return new \WP_Error( 'wpdlg_response_error', 'The user could not be retrieved by their email address.' );
+			}
+		} else {
+
+			return new \WP_Error( 'wpdlg_response_error', 'An invalid response was returned when trying to find the user by email address.' );
+		}
+	}
 }
